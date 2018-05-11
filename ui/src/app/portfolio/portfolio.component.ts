e0d2df2dcf468fa12a4483cafe6d9a45d6ee463b
Vincent La <vincela15@gmail.com>
2018-05-11 14:10:16 -0700

    reformating input boxes as a list

--- ui/src/app/portfolio/portfolio.component.ts
+++ ui/src/app/portfolio/portfolio.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  private usernameText: string = "Username"
+  private firstnameText: string = "First Name"
+  private lastnameText: string = "Last Name"
+  private portfolioImg: string;
+  private username: string;
+  private firstname: string;
+  private lastname: string;
 
   constructor() { }
 
