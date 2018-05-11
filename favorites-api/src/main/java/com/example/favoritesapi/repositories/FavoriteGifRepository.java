d25a7b4c9b878660421e138c8f4678b564b57edf
Vincent La <vincela15@gmail.com>
2018-05-11 14:32:53 -0700

    revmoing faovirtes-api service

--- favorites-api/src/main/java/com/example/favoritesapi/repositories/FavoriteGifRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.favoritesapi.repositories;
-
-import com.example.favoritesapi.models.FavoriteGif;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FavoriteGifRepository extends CrudRepository<FavoriteGif, Long> {
-
-}
