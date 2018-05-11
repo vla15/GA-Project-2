1e7bebbc9e608714127c022a17df381885332151
Vincent La <vincela15@gmail.com>
2018-05-11 11:28:23 -0700

    (feat)login-page
    user now is able to click login and be redirected to portfolio page

---
+++ ui/src/app/login-page/login-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  private usernameText: string = "Username: "
+  private passwordText: string = "Password: "
+  private username: string;
+  private password: string;
+  private dummyPassword: string = "";
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+
+  private handlePasswordInput(e) {
+  }
+
+  private handleLogin() {
+    this.router.navigate(["/portfolio"])
+  }
+
+}
