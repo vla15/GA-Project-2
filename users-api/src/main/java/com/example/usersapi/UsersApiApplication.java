0404026cc0ae12cdec4c04e5c0e1b4d5881b1fab
Vincent La <vincela15@gmail.com>
2018-05-11 20:54:41 -0700

    fixing docker file

--- users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
+++ users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
@@ -3,12 +3,9 @@ package com.example.usersapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
